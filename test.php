b6ff460fe9a2ab3e8590304dc330d541b76c127d
ludoc0de <champi.ludo@tutanota.com>
2025-04-19 14:14:11 -0300

    array test

--- test.php
+++ test.php
@@ -1,3 +1,56 @@
+<?php
+$users = [
+    [
+        'full_name' => 'Lisa J',
+        'email' => 'lisa.j@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Ludo C',
+        'email' => 'ludo.c@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
+for($lines=0; $lines <= 2 ; $lines++){
+     echo $users[$lines]['full_name'] . ' ' . $users[$lines]['email'] . '<br />';
+}
+
+$videos = [
+    [
+        'title' => 'Introduction à la Cyber',
+        'link' => 'https://www.google.com',
+        'author' => 'lisa.j@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Équipe bleu!',
+        'link' => 'https://www.google.com',
+        'author' => 'lisa.j@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+       'title' => 'Équipe rouge!',
+        'link' => 'https://www.google.com',
+        'author' => 'lisa.j@exemple.com',
+        'is_enabled' => true,
+    ],
+];
+
+foreach($videos as $video){
+
+    if(array_key_exists("is_enabled", $video) && $video["is_enabled"] === true){
+         echo "La clé 'is_enabled' se trouve dans '{$video['title']}' !";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
