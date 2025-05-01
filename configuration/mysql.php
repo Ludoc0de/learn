bd4ad2718f2ba8e620e503df262149ed51e19822
ludoc0de <champi.ludo@tutanota.com>
2025-05-01 17:21:13 -0300

    add

--- configuration/mysql.php
+++ configuration/mysql.php
@@ -1,25 +1,7 @@
 <?php
-try {
-    $mysqlClient = new PDO('mysql:host=localhost;dbname=learn_cyber;charset=utf8', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
 
-// On récupère tout le contenu de la table tutorials
-$sqlQuery = "SELECT titlee, link, author FROM tutorials WHERE author =:author AND is_enabled = :is_enabled";
-$tutorialsStatement = $mysqlClient->prepare($sqlQuery);
-$tutorialsStatement->execute([
-    'author' => 'lisa.cyber97@google.com',
-    'is_enabled' => 1,
-]);
-$tutorials = $tutorialsStatement->fetchAll();
-
-// On affiche chaque recette une à une
-foreach ($tutorials as $tutorial) {
-?>
-<h3><?php echo $tutorial['title']; ?></h3>
-<p><?php echo $tutorial['link']; ?></p>
-<a><?php echo $tutorial['author']; ?></a>
-</br>
-<?php
-}
+const MYSQL_HOST = localhost;
+const MYSQL_PORT = 3306;
+const MYSQL_NAME = lol;
+const MYSQL_USER = lol;
+const MYSQL_PASSWORD = lol;SQL
