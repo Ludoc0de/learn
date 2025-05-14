df5f9ab04f595c5f67ff5452ae86ab27bf8fc97f
ludoc0de <champi.ludo@tutanota.com>
2025-05-13 23:08:17 -0300

    starting router

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-session_start();
-// require_once(__DIR__ . '/variables.php');
-require('src/model.php');
 
-$availableTutorials = getTutorials();
-
-// $getAll = getAllTutorials($tutorials);
-
-require('templates/homepage.php');
+require_once('src/controllers/homepage.php');
+homepage();
