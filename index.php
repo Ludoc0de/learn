7f85923c9c1c9b0228efc77a9e86797e8a9fa8c8
ludoc0de <champi.ludo@tutanota.com>
2025-05-16 00:33:19 -0300

    login() in router

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-
+session_start();
 require_once('src/controllers/homepage.php');
-homepage();
+require_once('src/controllers/submit_login.php');
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'login') {
+        login();
+    }
+} else {
+    homepage();
+}
