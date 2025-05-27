67453f36060dc5a295b8f810dcc4745321cb43ad
ludoc0de <champi.ludo@tutanota.com>
2025-05-26 22:36:03 -0300

    review condition

--- src/controllers/back.php
+++ src/controllers/back.php
@@ -3,19 +3,23 @@ require_once('src/model.php');
 
 function addTutorial()
 {
-    $postData = $_POST;
     $alertMessage = null;
-    if (
-        isset($postData['title'])
-        || isset($postData['link'])
-    ) {
-        $alertMessage = 'Il faut renseigner le formulaire pour le soumettre.';
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $postData = $_POST;
+        if (
+            !isset($postData['title'])
+            || !isset($postData['link'])
+            || empty($postData['title'])
+            || empty($postData['link'])
+        ) {
+            $alertMessage = 'Il faut renseigner le formulaire pour le soumettre.';
+        } else {
+            $title = $postData['title'];
+            $link = $postData['link'];
+            $author =  $_SESSION["LOGGED_USER"]["email"];
+            $is_enabled = isset($postData['is_enabled']) ? 1 : 0;
+            createTutorials($title,  $link, $author, $is_enabled);
+        }
     }
-    // $title = $postData['title'];
-    // $link = $postData['link'];
-    // $author =  $_SESSION["LOGGED_USER"]["email"];
-    // $is_enabled = isset($postData['is_enabled']) ? 1 : 0;
-
-    // createTutorials($title,  $link, $author, $is_enabled);
     require('templates/back/create_tutorial.php');
 }
