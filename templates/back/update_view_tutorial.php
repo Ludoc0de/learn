69cf2fca03d17584a471416ca8df390fa9e1a4a1
ludoc0de <champi.ludo@tutanota.com>
2025-06-02 18:50:28 -0300

    update view  getTutorialById

---
+++ templates/back/update_view_tutorial.php
@@ -0,0 +1,47 @@
+<!-- 
+session_start();
+require_once __DIR__ . '/databaseconnect.php';
+$getData = $_GET;
+
+if (!isset($getData['id']) || !is_numeric($getData['id'])) {
+    echo ('Il faut un identifiant de tutoriel pour la modifier.');
+    return;
+}
+
+$retrieveTutorialStatement = $mysqlClient->prepare('SELECT * FROM tutorials WHERE tutorial_id = :id');
+$retrieveTutorialStatement->execute([
+    'id' => (int)$getData['id'],
+]);
+$tutorial = $retrieveTutorialStatement->fetch(PDO::FETCH_ASSOC);
+
+
+?> -->
+<?php ob_start(); ?>
+<?php ob_start(); ?>
+
+<div class="container">
+    <?php require_once(__DIR__ . '/header.php'); ?>
+    <h1>Mettre à jour un tutoriel</h1>
+    <form action="/learn/index.php?action=updateTutorial" method="POST">
+        <div>
+            <label for="id">Identifiant du tutoriel</label>
+            <input type="hidden" id="id" name="id" value="<?php echo ($tutorial['tutorial_id']); ?>">
+        </div>
+        <div>
+            <label for="title">Titre du tutoriel</label>
+            <input type="text" id="title" name="title" value=" <?php echo htmlspecialchars($tutorial['title']); ?>">
+        </div>
+        <div>
+            <label for="link">liens de la video</label>
+            <input type="text" id="link" name="link" value="<?php echo htmlspecialchars($tutorial['link']); ?>">
+        </div>
+        <div>
+            <label for=" is_enabled">Rendre visible</label>
+            <input type="checkbox" name="is_enabled" value="activé" value="<?php echo ($tutorial['is_enabled']); ?>">
+        </div>
+        <button type=" submit" class="btn btn-primary">Envoyer</button>
+    </form>
+    <br />
+</div>
+<?php $content = ob_get_clean(); ?>
+<?php require('layout.php') ?>
