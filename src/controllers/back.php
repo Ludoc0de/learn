1b9756784150d55c322821ab4f70f3c0e2f46f2a
ludoc0de <champi.ludo@tutanota.com>
2025-06-02 18:49:38 -0300

    controller or  getTutorialById

--- src/controllers/back.php
+++ src/controllers/back.php
@@ -27,27 +27,43 @@ function tutorials()
     $allTutorials = getAllTutorials();
     require('templates/back/get_all_tutorials.php');
 }
+//
+function getTutorialById()
+{
+    $alertMessage = null;
+    $getData = $_GET;
+    if (!isset($getData['id']) || !is_numeric($getData['id'])) {
+        $alertMessage = 'Il faut un identifiant de tutoriel pour la modifier.';
+        return;
+    }
+    $id = $getData['id'];
+    $tutorial = getTutorialByIdInDB($id);
 
+    require('templates/back/update_view_tutorial.php');
+}
+//
 function updateTutorial()
 {
     $alertMessage = null;
-    $postData = $_POST;
-    if (
-        !isset($postData['title']) || trim($postData['title']) === '' ||
-        !isset($postData['link']) || trim($postData['link']) === ''
-    ) {
-        $alertMessage = 'Il manque des informations dans le formulaire pour le soumettre.';
-        return;
-    } else {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $postData = $_POST;
+        if (
+            !isset($postData['title']) || trim($postData['title']) === '' ||
+            !isset($postData['link']) || trim($postData['link']) === ''
+        ) {
+            $alertMessage = 'Il manque des informations dans le formulaire pour le soumettre.';
+            return;
+        }
         $id = $postData['id'];
         $title = $postData['title'];
         $link = $postData['link'];
         $author =  $_SESSION["LOGGED_USER"]["email"];
         $is_enabled = isset($postData['is_enabled']) ? 1 : 0;
 
-        updateTutorialInDB($title,  $link, $is_enabled);
+        updateTutorialInDB($id, $title,  $link, $is_enabled);
     }
-    require('templates/back/update_tutorial.php');
+
+    redirectToUrl('index.php?action=tutorials');
 }
 
 function logout()
