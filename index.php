9f4ae32a0230f7cb836e48f7d9bc9066a9dbf073
ludoc0de <champi.ludo@tutanota.com>
2025-06-04 22:45:07 -0300

    logged to see admin route

--- index.php
+++ index.php
@@ -11,15 +11,35 @@ if (isset($_GET['action']) && $_GET['action'] !== "") {
             login();
         } elseif ($_GET['action'] == 'contact') {
             contact();
-        } elseif ($_GET['action'] == 'addTutorial') {
+        }
+        // ADMIN
+        elseif ($_GET['action'] == 'addTutorial') {
+            if (!isset($_SESSION['LOGGED_USER'])) {
+                redirectToUrl('index.php?action=login');
+            }
             addTutorial();
         } elseif ($_GET['action'] == 'tutorials') {
+            if (!isset($_SESSION['LOGGED_USER'])) {
+                redirectToUrl('index.php?action=login');
+            }
             tutorials();
         } elseif ($_GET['action'] == 'getTutorialId') {
+            if (!isset($_SESSION['LOGGED_USER'])) {
+                redirectToUrl('index.php?action=login');
+            }
+            // if (isset($_GET['id']) && $_GET['id'] > 0)
             getTutorialById();
         } elseif ($_GET['action'] == 'updateTutorial') {
+            if (!isset($_SESSION['LOGGED_USER'])) {
+                redirectToUrl('index.php?action=login');
+            }
+            // if (isset($_GET['id']) && $_GET['id'] > 0)
             updateTutorial();
         } elseif ($_GET['action'] == 'deleteTutorial') {
+            if (!isset($_SESSION['LOGGED_USER'])) {
+                redirectToUrl('index.php?action=login');
+            }
+            // if (isset($_GET['id']) && $_GET['id'] > 0)
             deleteTutorial();
         } elseif ($_GET['action'] == 'logout') {
             logout();
