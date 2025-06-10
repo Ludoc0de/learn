c994dd243bcf80958c9032c9447671218c8f062b
ludoc0de <champi.ludo@tutanota.com>
2025-06-09 22:15:39 -0300

    add try cath

--- index.php
+++ index.php
@@ -3,66 +3,71 @@ session_start();
 require_once('src/controllers/front.php');
 require_once('src/controllers/back.php');
 // require_once('src/controllers/submit_login.php');
-if (isset($_GET['action']) && $_GET['action'] !== "") {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'home') {
-            homepage();
-        } elseif ($_GET['action'] == 'login') {
-            login();
-        } elseif ($_GET['action'] == 'contact') {
-            contact();
-        }
-        // ADMIN
-        // access only if connected
-        elseif ($_GET['action'] == 'admin') {
-            if (!isset($_SESSION['LOGGED_USER'])) {
-                redirectToUrl('index.php?action=login');
+try {
+    //code...
+    if (isset($_GET['action']) && $_GET['action'] !== "") {
+        if (isset($_GET['action'])) {
+            if ($_GET['action'] == 'home') {
+                homepage();
+            } elseif ($_GET['action'] == 'login') {
+                login();
+            } elseif ($_GET['action'] == 'contact') {
+                contact();
             }
-            adminPage();
-        } elseif ($_GET['action'] == 'createTutorial') {
-            if (!isset($_SESSION['LOGGED_USER'])) {
-                redirectToUrl('index.php?action=login');
-            }
-            createTutorial();
-        } elseif ($_GET['action'] == 'tutorials') {
-            if (!isset($_SESSION['LOGGED_USER'])) {
-                redirectToUrl('index.php?action=login');
-            }
-            tutorials();
-        } elseif ($_GET['action'] == 'getTutorialId') {
-            if (!isset($_SESSION['LOGGED_USER'])) {
-                redirectToUrl('index.php?action=login');
-            } elseif (isset($_GET['id']) && $_GET['id'] > 0) {
-                $tutorialId = (int) $_GET['id'];
-                getTutorialById($tutorialId);
+            // ADMIN
+            // access only if connected
+            elseif ($_GET['action'] == 'admin') {
+                if (!isset($_SESSION['LOGGED_USER'])) {
+                    redirectToUrl('index.php?action=login');
+                }
+                adminPage();
+            } elseif ($_GET['action'] == 'createTutorial') {
+                if (!isset($_SESSION['LOGGED_USER'])) {
+                    redirectToUrl('index.php?action=login');
+                }
+                createTutorial();
+            } elseif ($_GET['action'] == 'tutorials') {
+                if (!isset($_SESSION['LOGGED_USER'])) {
+                    redirectToUrl('index.php?action=login');
+                }
+                tutorials();
+            } elseif ($_GET['action'] == 'getTutorialId') {
+                if (!isset($_SESSION['LOGGED_USER'])) {
+                    redirectToUrl('index.php?action=login');
+                } elseif (isset($_GET['id']) && $_GET['id'] > 0) {
+                    $tutorialId = (int) $_GET['id'];
+                    getTutorialById($tutorialId);
+                } else {
+                    throw new Exception('Identifiant de tutoriel manquant ou invalide.');
+                }
+            } elseif ($_GET['action'] == 'updateTutorial') {
+                if (!isset($_SESSION['LOGGED_USER'])) {
+                    redirectToUrl('index.php?action=login');
+                } elseif (isset($_GET['id']) && $_GET['id'] > 0) {
+                    $tutorialId = (int) $_GET['id'];
+                    updateTutorial($tutorialId);
+                } else {
+                    throw new Exception('Identifiant de tutoriel manquant ou invalide.');
+                }
+            } elseif ($_GET['action'] == 'deleteTutorial') {
+                if (!isset($_SESSION['LOGGED_USER'])) {
+                    redirectToUrl('index.php?action=login');
+                } elseif (isset($_GET['id']) && $_GET['id'] > 0) {
+                    $tutorialId = (int) $_GET['id'];
+                    deleteTutorial($tutorialId);
+                } else {
+                    throw new Exception('Identifiant de tutoriel manquant ou invalide.');
+                }
+            } elseif ($_GET['action'] == 'logout') {
+                logout();
             } else {
-                echo 'Identifiant de tutoriel manquant ou invalide.';
+                throw new Exception("Cette page n'existe pas.");
             }
-        } elseif ($_GET['action'] == 'updateTutorial') {
-            if (!isset($_SESSION['LOGGED_USER'])) {
-                redirectToUrl('index.php?action=login');
-            } elseif (isset($_GET['id']) && $_GET['id'] > 0) {
-                $tutorialId = (int) $_GET['id'];
-                updateTutorial($tutorialId);
-            } else {
-                echo 'Identifiant de tutoriel manquant ou invalide.';
-            }
-        } elseif ($_GET['action'] == 'deleteTutorial') {
-            if (!isset($_SESSION['LOGGED_USER'])) {
-                redirectToUrl('index.php?action=login');
-            } elseif (isset($_GET['id']) && $_GET['id'] > 0) {
-                $tutorialId = (int) $_GET['id'];
-                deleteTutorial($tutorialId);
-            } else {
-                echo 'Identifiant de tutoriel manquant ou invalide.';
-            }
-        } elseif ($_GET['action'] == 'logout') {
-            logout();
         }
     } else {
-        echo ("dont exist");
+        homepage();
     }
-} else {
-    homepage();
+} catch (Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
 }
 // f
