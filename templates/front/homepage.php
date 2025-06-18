e8856c98e38ed53b6f9c1cb7df3abc8855da96ed
ludoc0de <champi.ludo@tutanota.com>
2025-06-17 22:29:58 -0300

    up

--- templates/front/homepage.php
+++ templates/front/homepage.php
@@ -5,16 +5,30 @@
         <?php foreach (
             $availableTutorials as $tutorial
         ) { ?>
-        <h5>
-            <?php echo $tutorial['title']; ?>
-        </h5>
-        <a>
-            <?php echo $tutorial['link']; ?>
-        </a>
-        <i>by <?php echo getAuthors($tutorial['author']); ?> </i>
+        <div class="flex flex-1 card w-96 bg-base-200 rounded-sm card-md shadow-sm m-10">
+            <div class="card-body">
+                <h2 class="card-title"> <?php echo $tutorial['title']; ?></h2>
+                <a> <?php echo $tutorial['link']; ?></a>
+                <div class="justify-end card-actions">
+                    <i>Par <?php echo getAuthors($tutorial['author']); ?></i>
+                </div>
+            </div>
+        </div>
         <?php } ?>
+        <!-- <?php foreach (
+                    $availableTutorials as $tutorial
+                ) { ?>
+            <div class="flex flex-col flex-1 bg-base-300 rounded-sm m-2 p-4">
+                <h5 class="flex">
+                <?php echo $tutorial['title']; ?>
+            </h5>
+            <a>
+                <?php echo $tutorial['link']; ?>
+            </a>
+            <i>by <?php echo getAuthors($tutorial['author']); ?> </i>
+        </div>
+        <?php } ?> -->
     </div>
-
     <!-- <h1>Ici on s'initie à la Cyber!</h1>
     <?php foreach (
         $availableTutorials as $tutorial
