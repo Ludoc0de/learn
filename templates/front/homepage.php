e598ff9a92f8aa1f086fea038b22e882db6c2c63
ludoc0de <champi.ludo@tutanota.com>
2025-06-17 23:20:00 -0300

    add text and hero

--- templates/front/homepage.php
+++ templates/front/homepage.php
@@ -1,11 +1,22 @@
 <?php ob_start(); ?>
-<div>
-    <h1>Ici on s'initie à la Cyber!</h1>
-    <div class="flex flex-row">
+<div class="px-5">
+    <!-- <h1>Ici on s'initie à la Cyber!</h1> -->
+    <div class="hero bg-base-200 rounded-sm min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Ici on s'initie à la Cyber!</h1>
+                <p class="py-6">
+                    "Mieux comprendre la cybers sécurité, c'est mieux protéger son identité numérique."
+                </p>
+                <a class="btn btn-primary">Se connecter</a>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-row justify-between my-10">
         <?php foreach (
             $availableTutorials as $tutorial
         ) { ?>
-        <div class="flex flex-1 card w-96 bg-base-200 rounded-sm card-md shadow-sm m-10">
+        <div class="card w-96 bg-base-200 rounded-sm card-md shadow-sm">
             <div class="card-body">
                 <h2 class="card-title"> <?php echo $tutorial['title']; ?></h2>
                 <a> <?php echo $tutorial['link']; ?></a>
