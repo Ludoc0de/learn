1075951a5c77aca16de80ffa3c902884ca7e4959
ludoc0de <champi.ludo@tutanota.com>
2025-06-18 22:26:18 -0300

    work on css

--- templates/back/admin_page.php
+++ templates/back/admin_page.php
@@ -1,5 +1,36 @@
 <?php ob_start(); ?>
-<div>
+<div class="px-5">
+    <div class="hero bg-base-200 rounded-sm min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Page administrateur!</h1>
+                <p class="py-6">
+                    "Mieux comprendre la cybers sécurité, c'est mieux protéger son identité numérique."
+                </p>
+                <a href="index.php?action=login" class=" btn btn-primary">Éditer les tutos
+
+                </a>
+            </div>
+        </div>
+        <img src="public/images/arrow.svg" alt="flèche" class="relative top-50 left-32 animate-pulse">
+    </div>
+    <div class=" flex flex-col justify-between my-10 lg:flex-row">
+        <?php foreach (
+            $availableTutorials as $tutorial
+        ) { ?>
+        <div class="card w-96 bg-base-200 rounded-sm card-md shadow-sm my-6 lg:my-0">
+            <div class="card-body">
+                <h2 class="card-title"> <?php echo $tutorial['title']; ?></h2>
+                <a> <?php echo $tutorial['link']; ?></a>
+                <div class="justify-end card-actions">
+                    <i>Par <?php echo getAuthors($tutorial['author']); ?></i>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+</div>
+<!-- <div>
     <h1>Page administrateur!</h1>
     <?php foreach (
         $availableTutorials as $tutorial
@@ -13,6 +44,6 @@
         </br>
         <i>by <?php echo getAuthors($tutorial['author']); ?>
         <?php } ?>
-</div>
+</div> -->
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
