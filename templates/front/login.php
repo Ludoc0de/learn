99b592409cda8dc5882ca05f70c98e07fbd92415
ludoc0de <champi.ludo@tutanota.com>
2025-06-19 20:43:09 -0300

    add css

--- templates/front/login.php
+++ templates/front/login.php
@@ -1,30 +1,53 @@
-<?php if (!isset($_SESSION["LOGGED_USER"])) { ?>
-<h1>Connectez vous</h1>
-<form action="/learn/index.php?action=login" method="POST">
-    <!-- si message d'erreur on l'affiche -->
-    <?php
-        if ($alertMessage) {
-        ?>
-    <div class="alert alert-danger">
-        <?= $alertMessage ?>
+<?php ob_start();
+if (!isset($_SESSION["LOGGED_USER"])) { ?>
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <div class="text-center lg:text-left">
+            <h1 class="text-5xl font-bold">Se connecter!</h1>
+            <p class="py-6">
+                Bienvennue sur l'espace d'authentification! <br>
+                Ajouter vos Identifiants afin de vous connecter en tant qu'utilisateur enregistré à notre platforme.
+            </p>
+        </div>
+        <form action="/learn/index.php?action=login" method="POST">
+            <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div class="card-body">
+                    <fieldset class="fieldset">
+                        <label class="label">Email</label>
+                        <input type="email" id="email" name="email" class="input" placeholder="Email" />
+                        <label class="label">Mot de passe</label>
+                        <input type="password" id="password" name="password" class="input" placeholder="Password" />
+                        <!-- si message d'erreur on l'affiche -->
+                        <?php
+                            if ($alertMessage) {
+                            ?>
+                        <div class="alert alert-danger">
+                            <?= $alertMessage ?>
+                        </div>
+                        <?php
+                            }
+                            ?>
+                        <button type="submit" class="btn btn-neutral mt-4">Se connecter</button>
+                    </fieldset>
+                </div>
+            </div>
+        </form>
     </div>
-    <?php
-        }
-        ?>
-    <div>
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" aria-describedby="email-help">
-    </div>
-    <div>
-        <label for="password">Mot de passe</label>
-        <input type="password" id="password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Connecter</button>
-</form>
-<?php } else { ?>
-<div>
-    <h3>
-        Bienvennue <?php echo $_SESSION["LOGGED_USER"]["full_name"]; ?> sur le site!
-    </h3>
 </div>
-<?php }; ?>
+<?php } else { ?>
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <div>
+            <h1 class="text-5xl font-bold">Bienvennue <?php echo $_SESSION["LOGGED_USER"]["full_name"]; ?> sur le site!
+            </h1>
+            <p class="py-6 text-2xl">
+                Tu es déjà connecté!
+            </p>
+            <a href="/learn/index.php?action=admin" class="link text-xl">Retour sur la page Admin</a>
+        </div>
+    </div>
+</div>
+
+<?php };
+$content = ob_get_clean();
+require('layout.php') ?>
