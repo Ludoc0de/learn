5946b90cd667953afe404b655f0de02b5befead0
ludoc0de <champi.ludo@tutanota.com>
2025-06-21 18:57:19 -0300

    clean

--- templates/back/get_all_tutorials.php
+++ templates/back/get_all_tutorials.php
@@ -37,34 +37,6 @@
         </div>
         <?php } ?>
     </div>
-    <!--  -->
-    <h1>Administrer les tutoriels</h1>
-    <?php foreach ($allTutorials as $tutorial) { ?>
-    <h5>
-        <?php echo $tutorial['title']; ?>
-    </h5>
-    <a>
-        <?php echo $tutorial['link']; ?>
-    </a>
-    </br>
-    <i> by <?php echo getAuthors($tutorial['author']); ?></i>
-
-    <div>
-        <ul>
-            <!-- add a condition only for admin acces  -->
-            <?php if (isset($_SESSION["LOGGED_USER"])) { ?>
-            <li>
-                <a href="/learn/index.php?action=getTutorialId&id=<?php echo ($tutorial['tutorial_id']); ?>">Éditer</a>
-            </li>
-            <li>
-                <a href="/learn/index.php?action=deleteTutorial&id=<?php echo ($tutorial['tutorial_id']); ?>"
-                    onclick="return confirm('attention suppression définitive !')">Supprimer</a>
-            </li>
-            <?php } ?>
-        </ul>
-    </div>
-
-    <?php } ?>
 </div>
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
