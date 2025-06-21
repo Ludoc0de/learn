fdca9105db4d07a62775068ad41ea15f9447a588
ludoc0de <champi.ludo@tutanota.com>
2025-06-21 18:17:42 -0300

    design

--- templates/back/create_tutorial.php
+++ templates/back/create_tutorial.php
@@ -1,31 +1,39 @@
 <?php ob_start(); ?>
-<div class="container">
-    <h1>Ajoutez un tutoriel</h1>
+<div>
+    <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Ajoutez un tutoriel</h1>
+        <p class="py-6">
+            Ajouter les nouvelles initiaions à la cyber!
+        </p>
+    </div>
     <form action="/learn/index.php?action=createTutorial" method="POST">
-        <?php
-        if ($alertMessage) {
-        ?>
-            <div class="alert alert-danger">
-                <?= $alertMessage ?>
+        <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div class="card-body">
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Titre du tutoriel</legend>
+                    <input type="text" id="title" class="input" name="title" placeholder="Titre ici" />
+                    <legend class="fieldset-legend">Liens de la video</legend>
+                    <input type="text" id="link" class="input" name="link" placeholder="Liens ici" />
+                    <legend class="fieldset-legend">Rendre le lien visible</legend>
+                    <label class="label">
+                        <input type="checkbox" checked="checked" class="checkbox" name="is_enabled" value="activé" />
+                        Visible!
+                    </label>
+                    <!-- if error message we can see -->
+                    <?php
+                    if ($alertMessage) {
+                    ?>
+                    <div class="alert alert-danger mt-4">
+                        <?= $alertMessage ?>
+                    </div>
+                    <?php
+                    }
+                    ?>
+                    <button type="submit" class="btn btn-primary mt-4">Envoyer</button>
+                </fieldset>
             </div>
-        <?php
-        }
-        ?>
-        <div>
-            <label for="title">Titre du tutoriel</label>
-            <input type="text" id="title" name="title">
         </div>
-        <div>
-            <label for="link">liens de la video</label>
-            <input type="text" id="link" name="link">
-        </div>
-        <div>
-            <label for="is_enabled">Rendre visible</label>
-            <input type="checkbox" name="is_enabled" value="activé">
-        </div>
-        <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
-    <br />
 </div>
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
