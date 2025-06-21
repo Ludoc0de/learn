5b7c2868ede7455d7a9d7f7335cccfc59dfbd872
ludoc0de <champi.ludo@tutanota.com>
2025-06-21 19:20:33 -0300

    design

--- templates/back/update_view_tutorial.php
+++ templates/back/update_view_tutorial.php
@@ -1,36 +1,45 @@
 <?php ob_start(); ?>
-<div class="container">
-    <h1>Mettre à jour un tutoriel</h1>
-    <?php
-    if ($alertMessage) {
-    ?>
-        <div class="alert alert-danger">
-            <h3><?= $alertMessage ?></h3>
+<div>
+    <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold"> Modifier à jour un tutoriel</h1>
+        <p class="py-6">
+            Modifier les liens d'initiaions à la cyber!
+        </p>
+    </div>
+    <form action="/learn/index.php?action=updateTutorial&id=<?php echo ($tutorial['tutorial_id']); ?>" method="POST">
+        <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div class="card-body">
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Identifiant du tutoriel</legend>
+                    <input type="hidden" id="id" name="id" class="input"
+                        value="<?php echo ($tutorial['tutorial_id']); ?>" />
+                    <legend class="fieldset-legend">Titre du tutoriel</legend>
+                    <input type="text" id="title" class="input" name="title"
+                        value="<?php echo htmlspecialchars($tutorial['title']); ?>" />
+                    <legend class="fieldset-legend">Liens de la video</legend>
+                    <input type="text" id="link" class="input" name="link"
+                        value="<?php echo htmlspecialchars($tutorial['link']); ?>" />
+                    <legend class="fieldset-legend">Rendre le lien visible</legend>
+                    <label class="label">
+                        <input type="checkbox" checked="checked" class="checkbox" name="is_enabled" value="activé"
+                            value="<?php echo ($tutorial['is_enabled']); ?>" />
+                        Visible!
+                    </label>
+                    <!-- if error message we can see -->
+                    <?php
+                    if ($alertMessage) {
+                    ?>
+                    <div class="alert alert-danger mt-4">
+                        <?= $alertMessage ?>
+                    </div>
+                    <?php
+                    }
+                    ?>
+                    <button type="submit" class="btn btn-primary mt-4">Envoyer</button>
+                </fieldset>
+            </div>
         </div>
-    <?php
-    } else { ?>
-        <form action="/learn/index.php?action=updateTutorial&id=<?php echo ($tutorial['tutorial_id']); ?>" method="POST">
-            <div>
-                <label for="id">Identifiant du tutoriel</label>
-                <input type="hidden" id="id" name="id" value="<?php echo ($tutorial['tutorial_id']); ?>">
-            </div>
-            <div>
-                <label for="title">Titre du tutoriel</label>
-                <input type="text" id="title" name="title" value="<?php echo htmlspecialchars($tutorial['title']); ?>">
-            </div>
-            <div>
-                <label for="link">liens de la video</label>
-                <input type="text" id="link" name="link" value="<?php echo htmlspecialchars($tutorial['link']); ?>">
-            </div>
-            <div>
-                <label for=" is_enabled">Rendre visible</label>
-                <input type="checkbox" name="is_enabled" value="activé" value="<?php echo ($tutorial['is_enabled']); ?>">
-            </div>
-            <button type=" submit" class="btn btn-primary">Envoyer</button>
-        </form>
-        <br />
+    </form>
 </div>
-<?php  }
-?>
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
